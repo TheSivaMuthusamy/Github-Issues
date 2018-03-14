480cfe98b926ca6941909bb61a00da084ff7512a
Siva Muthusamy <s.b.muthusamy@gmail.com>
2018-03-14 01:57:45 -0400

    Basic Functionality

--- mix.exs
+++ mix.exs
@@ -4,7 +4,10 @@ defmodule Issues.Mixfile do
   def project do
     [
       app: :issues,
+      escript: escript_config(),
       version: "0.1.0",
+      name: "Issues",
+      source_url: "https://github.com/TheSivaMuthusamy/Github-Issues",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
@@ -21,8 +24,16 @@ defmodule Issues.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      { :httpoison, "~> 0.9" },
+      {:poison,    "~> 2.2"},
+      {:ex_doc, "~> 0.12"},
+      {:earmark, "~> 1.0", overide: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
+  end
+
+  def escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
